800b711f03c846b13bd641bf771a1c7b87a0490b
charlie <kjell.gunnar.midtlyng@bekk.no>
2012-01-31 16:24:34 +0100

    Added POST-handling with parameters

--- project/src/main/java/com/tek271/reverseProxy/servlet/ProxyFilter.java
+++ project/src/main/java/com/tek271/reverseProxy/servlet/ProxyFilter.java
@@ -17,14 +17,27 @@ along with Tek271 Reverse Proxy Server.  If not, see http://www.gnu.org/licenses
 package com.tek271.reverseProxy.servlet;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.List;
 
 import javax.servlet.*;
 import javax.servlet.http.*;
 
 import org.apache.http.*;
 import org.apache.http.client.*;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpPut;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.entity.InputStreamEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.params.BasicHttpParams;
+import org.apache.http.params.HttpParams;
 
 import com.tek271.reverseProxy.model.Mapping;
 import com.tek271.reverseProxy.text.UrlMapper;
@@ -48,7 +61,7 @@ public class ProxyFilter implements Filter {
       return;
     }
     
-    executeRequest(response, mapped.e1, mapped.e2);
+    executeRequest(response, mapped.e1, mapped.e2, ((HttpServletRequest)request));
   }
   
   private static boolean isHttp(ServletRequest request, ServletResponse response) {
@@ -64,12 +77,38 @@ public class ProxyFilter implements Filter {
     return UrlMapper.mapFullUrlProxyToHidden(oldUrl);
   }
   
-  private static void executeRequest(ServletResponse response, Mapping mapping, String newUrl) 
+  /**
+   * Helper method for passing post-requests
+   */
+  private static HttpUriRequest createNewRequest(HttpServletRequest request, String newUrl) throws UnsupportedEncodingException{
+	  String method = request.getMethod();
+	  if (method.equals("POST")) {
+		  List<NameValuePair> formparams = new ArrayList<NameValuePair>();
+		  Enumeration<String> en = request.getParameterNames();
+		  while (en.hasMoreElements()){
+			  String name = en.nextElement();
+			  String value = request.getParameter(name);
+			  formparams.add(new BasicNameValuePair("json", value));
+		  }
+		  UrlEncodedFormEntity entity = new UrlEncodedFormEntity(formparams, "UTF-8");
+		  HttpPost httppost = new HttpPost(newUrl);
+		  httppost.setEntity(entity);
+		  return httppost;
+	  }
+	  else if (method.equals("PUT")){
+		  return new HttpPut(newUrl);
+	  }
+	  else {
+		  return new HttpGet(newUrl);
+	  }
+  }
+  
+  private static void executeRequest(ServletResponse response, Mapping mapping, String newUrl, HttpServletRequest request) 
           throws IOException, ClientProtocolException {
-    //System.out.println(newUrl);
-    HttpClient httpclient = new DefaultHttpClient();
-    HttpGet httpget = new HttpGet(newUrl);
-    HttpResponse r = httpclient.execute(httpget);
+	HttpClient httpclient = new DefaultHttpClient();
+	
+    HttpUriRequest httpRequest = createNewRequest(request, newUrl);
+    HttpResponse r = httpclient.execute(httpRequest);
     HttpEntity entity = r.getEntity();
     
     ContentTranslator contentTranslator= new ContentTranslator(mapping, newUrl);
